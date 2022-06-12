d585a005e739b42c6b597af118078a0163c8dc1f
Matthew Griffin <1matthewgriffin@gmail.com>
2022-06-12 02:34:22 -0500

    Add question mark

--- pterodactyl_execute.js
+++ pterodactyl_execute.js
@@ -38,7 +38,7 @@ function collectInputs(inputdir, f, options) {
 
 function writeOutput(outputdir, output, options) {
   const jsonOutput = JSON.stringify(output);
-  const outputName = options.outputName ?? "output0"
+  const outputName = options?.outputName ?? "output0"
   Deno.writeTextFileSync(`${outputdir}/${outputName}`, jsonOutput);
 }
 
