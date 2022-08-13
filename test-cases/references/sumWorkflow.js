f2e5f38d82f6999bc6d92972bb4b6037fe8b1dc4
Matthew Griffin <1matthewgriffin@gmail.com>
2022-08-13 01:21:36 -0500

    Fix formatting

--- test-cases/references/sumWorkflow.js
+++ test-cases/references/sumWorkflow.js
@@ -1,7 +1,7 @@
 import pterodactyl from "../../pterodactyl.js";
 
 const sumTask = pterodactyl.task(function sum(a, b) {
-  return a+b;
+  return a + b;
 });
 
 const sumWorkflow = pterodactyl.workflow(function sum(a, b) {
