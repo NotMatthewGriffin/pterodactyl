6453e5e31a3bff5f1af412b3d48cf401881c13fb
Matthew Griffin <1matthewgriffin@gmail.com>
2023-02-17 02:09:36 -0600

    Format example

--- examples/named_io/workflow.js
+++ examples/named_io/workflow.js
@@ -6,24 +6,24 @@ import {
 const sum = task(function sum(x, y) {
   return x + y;
 }, {
-	paramNames: ["x", "y"],
-	outputName: "result"
+  paramNames: ["x", "y"],
+  outputName: "result",
 });
 
 const square = task(function square(z) {
   return z * z;
 }, {
-	paramNames: ["z"],
-	outputName: "squared"
+  paramNames: ["z"],
+  outputName: "squared",
 });
 
 const sumOfSquares = workflow(function sumOfSquares(x, y) {
   return sum(square(x), square(y));
 }, {
-	paramNames: ["left", "right"], // these names will be used in flyte console
-	outputName: "sumOfSquares"
+  paramNames: ["left", "right"], // these names will be used in flyte console
+  outputName: "sumOfSquares",
 });
 
 if (import.meta.main) {
-	console.log(sumOfSquares(5, 6));
+  console.log(sumOfSquares(5, 6));
 }
