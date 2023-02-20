7080f4e82b7d32a0a004b7dbe92891d0f3330399
Matthew Griffin <1matthewgriffin@gmail.com>
2023-02-19 18:45:35 -0600

    Update to newest deno standard library

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ import {
   assert,
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.170.0/testing/asserts.ts";
+} from "https://deno.land/std@0.177.0/testing/asserts.ts";
 
 import { isSerializable, registerScriptWithOptions } from "./src/register.js";
 import { getSecret, Secret, SecretMountType } from "./pterodactyl.js";
